c4c0b10c82f445c21e96381752d9b975822000aa
prabhutejad <prabhuteja.iot@gmail.com>
2024-10-01 16:27:57 +0530

    ImageViewer Component added

--- public/src/features/Components/advtCardComponent.tsx
+++ public/src/features/Components/advtCardComponent.tsx
@@ -1,29 +1,33 @@
-const AdvtCardComponent=()=>{
-    return(
-        <>
-          <section className="block_4">
-      <div className="container py-5">
-      <div className="row">
-        <div className="col-lg-8 offset-lg-2">
-        <article className="d-md-flex justify-content-center bg-white p-3 p-md-5 shadow gap-5 align-items-center rounded-2">
-          <div>
-            <div>
-              <h1 className="pb-md-3 dark_color">Would you like to start a project with us?</h1>
-              <p>Etiam erat lectus, finibus eget commodo quis, tincidunt eget leo. Nullam quis vulputate orci, ac accumsan quam.
-              Morbi fringilla congue libero. </p>
+const AdvtCardComponent = () => {
+  return (
+    <>
+      <section className="block_4">
+        <div className="container py-5">
+          <div className="row">
+            <div className="col-lg-8 offset-lg-2">
+              <article className="d-md-flex justify-content-between bg-white p-4 p-md-5 shadow gap-5 align-items-center rounded-2">
+                <div>
+                  <h2 className="pb-md-3 dark_color">Would you like to start a project with us?</h2>
+                  <p>
+                    Etiam erat lectus, finibus eget commodo quis, tincidunt eget leo. Nullam quis
+                    vulputate orci, ac accumsan quam. Morbi fringilla congue libero.
+                  </p>
+                </div>
+                <div>
+                  <button className="btn_theme mt-2 mb-5 d-inline-flex w-auto justify-content-center align-items-center text-center">
+                    <span className="d-block">Get</span>&nbsp;
+                    <span className="d-block">Started</span>
+                  </button>
+
+
+                </div>
+              </article>
             </div>
           </div>
-          <div>
-          <span className="btn_theme mt-2 mb-5 d-inline-flex w-auto">Get Started</span>
-          </div>
-        </article>
         </div>
-      </div>
+      </section>
+    </>
+  );
+};
 
-      </div>
-        </section>
-
-        </>
-    )
-}
 export default AdvtCardComponent;
